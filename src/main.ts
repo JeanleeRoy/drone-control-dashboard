430e1848ae3f7d415839e782d1d5bc6dd1d4a3fc
Jeanlee Barreto <jeanlee-roy@hotmail.com>
2023-05-31 01:26:04 -0500

    chore: add query and head plugins

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 
 import { createApp } from "vue";
+import { query } from '~/plugins/query';
 import '~/assets/css/fonts.css';
 import '~/assets/css/tailwind.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
@@ -12,5 +13,6 @@ const app = createApp(App);
 
 app.use(pinia);
 app.use(router);
+app.use(query);
 
 app.mount("#app");
